54d2e2ae0ef3a0a0e3491bdce9f41c8193305b92
orsoli <fljcoli@gmail.com>
2025-03-31 13:27:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Home from "./pages/Home.vue";
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style lang="scss">
+@use "bootstrap/scss/bootstrap.scss" as *;
+@use "./style/general.scss" as *;
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Daily Trip Albania",
+    };
+  },
+
+  components: {},
+
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="img-bg">
+    <h1 class="text-light">{{ title }}</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+
+.img-bg {
+  width: 100vw;
+  height: 100vh;
+  background-image: url("src/assets/img/3_islands_ksamil_Albania.webp");
+  background-size: cover;
+  background-position: center;
+}
+</style>
